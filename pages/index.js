29209adf39ca6d38b3d80efe55cb98f540f903fb
Burak Özkan <burak.ozkan@path.com.tr>
2022-01-24 23:55:47 +0300

    episode3

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import { gql, GraphQLClient } from 'graphql-request';
+import Section from '../components/Section';
 
 export const getStaticProps = async () => {
 
   const token = process.env.NEXT_PUBLIC_TOKEN;
-  const url = process.envENDPOINT;
+  const url = process.env.ENDPOINT;
 
   const graphQLClient = new GraphQLClient(url, {
     headers: {
       'Authorization': `Bearer ${token}`
     }
-  }); 
+  });
 
   const query = gql`
 query {
@@ -45,14 +46,32 @@ query {
 }
 
 
-const Home = ({videos}) => {
+const Home = ({ videos }) => {
+
+  const randomVideo = (videos) => {
+    return videos[Math.floor(Math.random() * videos.length)]
+  }
 
   console.log(videos)
 
   return (
-    <div className=''>
-      Hello World
-    </div>
+    <>
+      <div className='main-video'>
+        <img src={randomVideo(videos).thumbnails.url} alt={randomVideo(videos).title} />
+      </div>
+      <div className="video-feed">
+        <Section genre={'Thriller'} />
+        <Section genre={'Pixar'} />
+        <Section genre={'Marvel'} />
+        <Section genre={'National Geographic'} />
+        <Section genre={'Disney'} />
+        <Section genre={'Classic'} />
+        <Section genre={'Star Wars'} />
+        <Section genre={'Star Wars'} />
+        <Section genre={'Star Wars'} />
+      </div>
+    </>
+
   )
 }
 
