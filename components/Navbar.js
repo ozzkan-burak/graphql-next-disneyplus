845461dc429b17de8b41aff0d3e0a9c77ec71fc2
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-22 23:46:41 +0300

    episode7

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,16 @@ import Link from "next/link";
 import logo from "../public/disney-logo.png";
 
 
-const Navbar = () => {
+const Navbar = ({account}) => {
   return (
     <nav>
       <Link href="/">
         <Image src={logo} alt="disney logo" width={90} height={50} />
       </Link>
+      <div className="account-info">
+        <p>Welcome {account.username}</p>
+        <img className="avatar" src={account.avatar.url} alt="avatar" />
+      </div>
     </nav>
   )
 }
